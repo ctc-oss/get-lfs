8621f05b2094459b37f19876cb5ce58925125f41
John Wass <wassj@ctc.com>
2019-08-16 23:45:18 -0400

    import git-lfs-fetch adapter

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 import os
 from setuptools import setup
+from version import get_version
 
 requires = []
 with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
     requires = f.read().splitlines()
 
 setup(name='get-lfs',
-      version='0.0.1',
+      version=get_version(),
       packages=['lfs'],
       install_requires=requires,
       include_package_data=True,
