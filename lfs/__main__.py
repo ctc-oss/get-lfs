af8494ef2d6c3fc79360b5683524c6b915f4ce21
John Wass <wassj@ctc.com>
2020-02-18 20:47:09 -0500

    add an insecure option to main

--- lfs/__main__.py
+++ lfs/__main__.py
@@ -10,7 +10,11 @@ if __name__ == "__main__":
     parser.add_argument("-r", "--ref", type=str, default='master', help="LFS data ref")
     parser.add_argument("-I", "--include", type=str, help="Inclusion pattern; empty for all")
     parser.add_argument("-X", "--exclude", type=str, help="Exclusion pattern; defaults to xview dict")
+    parser.add_argument("-k", "--insecure", action='store_true', help="Skip SSL verification; GIT_SSL_NO_VERIFY")
     args = parser.parse_args()
+
+    if args.insecure:
+        os.environ['GIT_SSL_NO_VERIFY'] = '1'
 
     includes = []
     if args.include:
