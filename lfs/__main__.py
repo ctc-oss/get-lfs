d8d5bfc0161c09a3194f962ed1bb98ecdfc5fee7
John Wass <wassj@ctc.com>
2020-03-19 16:12:20 -0400

    callable main

--- lfs/__main__.py
+++ lfs/__main__.py
@@ -1,17 +1,18 @@
-import os
-
 import argparse
+import os
+import sys
+
 import lfs
 
 
-if __name__ == "__main__":
+def main(*argv):
     parser = argparse.ArgumentParser()
     parser.add_argument("lfs_url", type=str, help="LFS repo URL")
     parser.add_argument("-r", "--ref", type=str, default='master', help="LFS data ref")
     parser.add_argument("-I", "--include", type=str, help="Inclusion pattern; empty for all")
     parser.add_argument("-X", "--exclude", type=str, help="Exclusion pattern; defaults to xview dict")
     parser.add_argument("-k", "--insecure", action='store_true', help="Skip SSL verification; GIT_SSL_NO_VERIFY")
-    args = parser.parse_args()
+    args = parser.parse_args(argv)
 
     if args.insecure:
         os.environ['GIT_SSL_NO_VERIFY'] = '1'
@@ -33,3 +34,7 @@ if __name__ == "__main__":
         f = lfs.checkout(args.lfs_url, args.ref, includes, excludes)
 
     print(f)
+
+
+if __name__ == "__main__":
+    main(*sys.argv[1:])
